e6ce92fdc22e66c5ea7574106219c6ac221f7e3e
maruichang <378025342@qq.com>
2019-04-24 10:13:07 +0800

    好的

---
+++ text_pojo/src/text/pojo/TectDome.java
@@ -0,0 +1,15 @@
+package text.pojo;
+
+public class TectDome {
+    public static void main(String[] args) {
+        System.out.println("66666666666666");
+        System.out.println("66666666666666");
+        System.out.println("66666666666666");
+        System.out.println("66666666666666");
+        System.out.println("66666666666666");
+        System.out.println("77777777");
+        System.out.println("88888888");
+
+
+    }
+}
